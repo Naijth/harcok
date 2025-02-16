eaa8a0737de56ef1ebaa8b9846a60a8daae28fa2
Naijth <163039373+Naijth@users.noreply.github.com>
2025-02-16 19:27:19 +0100

    feladat 2,3,4,5

--- functions.js
+++ functions.js
@@ -39,3 +39,68 @@ function renderTable(array, thead, tbody){
         }
     }
 }
+
+function validateFormField(inputElement, inputErrorMessage){
+    if (inputElement.value == ''){
+        const parentElement = inputElement.parentElement;
+        const error = parentElement.querySelector('.error');
+        if (error != undefined) {
+            error.innerHTML = inputErrorMessage;
+        }
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateTwoForces(inputSideElement, inputNumbersElement){
+    if (inputSideElement.value != '' && inputNumbersElement.value == ''){
+        return validateFormField(inputNumbersElement, "Mind két mezőt ki kell tölteni!");
+    } else if (inputSideElement.value == '' && inputNumbersElement.value != '') {
+        return validateFormField(inputSideElement, "Mind két mezőt ki kell tölteni!")
+    }
+    return true;
+}
+
+function renderForm() {
+    const form = document.createElement('form');
+    form.id = 'form';
+    form.action = '';
+    document.body.appendChild(form);
+
+    formField(form, "text", "Származás:", "harc_nev")
+    formField(form, "text", "1. Harcoló fél:", "harcolo1")
+    formField(form, "text", "1. Haderő:", "hadero1")
+    formField(form, "text", "2. Harcoló fél:", "harcolo2")
+    formField(form, "text", "2. Haderő:", "hadero2")
+
+    const button = document.createElement('button');
+    button.innerHTML = "Hozzáadás";
+    form.appendChild(button);
+}
+
+function formField(form, type, labelText, id){
+    const mainDiv = document.createElement('div');
+    form.appendChild(mainDiv);
+
+    const label = document.createElement('label');
+    label.for = id;
+    label.innerHTML = labelText;
+    mainDiv.appendChild(label);
+    const br1 = document.createElement('br');
+    mainDiv.appendChild(br1);
+
+    const input = document.createElement('input');
+    input.type = type;
+    input.id = id;
+    input.name = id;
+    mainDiv.appendChild(input);
+    const br2 = document.createElement('br');
+    mainDiv.appendChild(br2);
+
+    const errorDiv = document.createElement('div');
+    errorDiv.setAttribute('class', 'error'); 
+    mainDiv.appendChild(errorDiv);
+    const br3 = document.createElement('br');
+    mainDiv.appendChild(br3);
+}
--- main.js
+++ main.js
@@ -32,6 +32,8 @@ const array = [
     }
 ]
 
+renderForm();
+
 const table = document.createElement('table');
 document.body.appendChild(table);
 const thead = document.createElement('thead');
@@ -40,3 +42,55 @@ const tbody = document.createElement('tbody');
 table.appendChild(tbody);
 
 renderTable(array, thead, tbody);
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let valid = true;
+
+    const warElement = document.getElementById("harc_nev");
+    const side1Element = document.getElementById("harcolo1");
+    const numbers1Element = document.getElementById("hadero1");
+    const side2Element = document.getElementById("harcolo2");
+    const numbers2Element = document.getElementById("hadero2");
+
+    const war = warElement.value;
+    const side1 = side1Element.value;
+    const numbers1 = numbers1Element.value;
+    const side2 = side2Element.value;
+    const numbers2 = numbers2Element.value;
+
+    const valid1 = validateFormField(warElement, "Ezt a mezőt kötelező kitölteni!");
+    const valid2 = validateFormField(side1Element, "Ezt a mezőt kötelező kitölteni!");
+    const valid3 = validateFormField(numbers1Element, "Ezt a mezőt kötelező kitölteni!");
+    const valid4 = validateTwoForces(side2Element, numbers2Element);
+
+    if (!valid1 || !valid2 || !valid3 || !valid4)
+        valid = false;
+
+    const twoForces = validateTwoForces(side2Element, numbers2Element);
+
+    if (valid && !twoForces) {
+        const newElement = {
+            war: war,
+            side1: side1,
+            numbers1: numbers1
+        }
+        array.push(newElement);
+        tbody.innerHTML = '';
+        renderTable(array, thead, tbody);
+        form.reset();
+    } else if (valid && twoForces) {
+        const newElement = {
+            war: war,
+            side1: side1,
+            numbers1: numbers1,
+            side2: side2,
+            numbers2: numbers2
+        }
+        array.push(newElement);
+        tbody.innerHTML = '';
+        renderTable(array, thead, tbody);
+        form.reset();
+    }
+});
